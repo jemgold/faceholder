92c7d0b4dc0d847efe134bb9a140fffa471c70b8
Jon Gold <hello@designedbygold.com>
2013-05-02 14:58:22 +0100

    More stuff

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
+require 'rack/cache'
 require './image_resizer'
-require 'pry'
+# require 'pry'
+
 
 class SpaceHolder < Sinatra::Base
+  use Rack::Cache, :metastore => 'file:tmp/cache/meta', :entitystore => 'file:tmp/cache/body'
+
   set :public_folder, File.dirname(__FILE__) + '/static'
+
+  configure do
+    set :images, Dir.glob('raw/top100/*.jpg')
+  end
 
   get '/' do
     erb :index
@@ -15,9 +23,10 @@ class SpaceHolder < Sinatra::Base
 
   get '/p/:width' do
     width = params[:width].to_i
-    path = Dir.glob('raw/top100/*.jpg').shuffle[0]
+    path = settings.images.shuffle[0]
     image = ImageResizer.new(path)
     image.width= width
+    cache_control :public, :max_age => 5
     content_type("image/#{image.image_format}")
     image.to_s
   end
