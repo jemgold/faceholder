c57bd684013a9ff8c0567cae948359e4535360d1
Jon Gold <hello@designedbygold.com>
2013-05-06 23:15:24 +0100

    Update widths finder

--- app.rb
+++ app.rb
@@ -4,30 +4,38 @@ require './image_resizer'
 # require 'pry'
 
 
-class SpaceHolder < Sinatra::Base
+class Faceholder < Sinatra::Base
   use Rack::Cache, :metastore => 'file:tmp/cache/meta', :entitystore => 'file:tmp/cache/body'
 
   set :public_folder, File.dirname(__FILE__) + '/static'
 
-  configure do
-    set :images, Dir.glob('raw/*.{jpg,png}')
-  end
+  set :image_widths, -> { Dir.glob('raw/**').map {|dir| dir.gsub(/raw\//, '').to_i }.sort }
 
   get '/' do
     erb :index
   end
 
-  get '/p' do
-    redirect '/p/100'
-  end
-
-  get '/p/:width' do
-    width = params[:width].to_i
-    path = settings.images.shuffle[0]
+  get '/:width' do
+    target_width = params[:width].to_i
+    base_width = find_a_width(target_width)
+    path = Dir.glob("raw/#{base_width}/*.{jpg,png}").shuffle[0]
     image = ImageResizer.new(path)
-    image.width= width
-    cache_control :public, :max_age => 5
+    image.width= target_width
+    # TODO: turn me back on
+    # cache_control :public, :max_age => 5
     content_type("image/#{image.image_format}")
     image.to_s
   end
+
+  def find_a_width(width)
+    arr = settings.image_widths
+    arr.each do |w|
+      if width <= w
+        break w
+      else
+        return arr.last
+      end
+    end
+  end
+
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
 require './app.rb'
-run SpaceHolder
+run Faceholder
